fc88dbd5a4caf0f01de4fffdaa58498330e80f81
柴晓科 <chaixiaoke@58.com>
2024-04-19 11:28:09 +0800

    fix: 恢复

--- src/axios/index.js
+++ src/axios/index.js
@@ -36,7 +36,7 @@ export default class Axios {
             JsonP(options.url, {
                 param: 'callback'
             }, function (err, response) {
-                if (response.status == 'success') {
+                if (response && response.status == 'success') {
                     resolve(response);
                 } else {
                     reject(response.messsage);
